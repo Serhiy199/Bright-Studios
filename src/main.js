4ebcf2de9c0381a6dfe8fb9f03256ead07a2fcb7
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2024-11-27 22:31:52 +0200

    head-rakuten

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -19,6 +18,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#counter'));
